51d6253841ab68bf5eb5b19e27582fff4b376df4
acly7946 <acly.work@tutanota.com>
2022-06-05 16:18:29 +0000

    Add yes subprojects

---
+++ subprojects/yes/completion/bash/yes.bash
@@ -0,0 +1,30 @@
+# yes(1) completion
+
+_yes()
+{
+	local cur short long
+	_get_comp_words_by_ref cur
+
+	short=(
+		-h
+		-V
+	)
+
+	long=(
+		--help
+		--version
+	)
+
+	if [[ $cur == --* ]]; then
+		COMPREPLY=($(compgen -W "${long[*]}" -- "$cur"))
+	elif [[ $cur == -* ]]; then
+		COMPREPLY=($(compgen -W "${short[*]}" -- "$cur"))
+		COMPREPLY+=($(compgen -W "${long[*]}" -- "$cur"))
+	else
+		COMPREPLY=($(compgen -W "${short[*]}" -- "$cur"))
+		COMPREPLY+=($(compgen -W "${long[*]}" -- "$cur"))
+		COMPREPLY+=($(compgen -c -- "$cur"))
+	fi
+
+} &&
+complete -F _yes uname
---
+++ subprojects/yes/completion/fish/yes.fish
@@ -0,0 +1,5 @@
+# yes(1) completion
+
+complete -f -c yes
+complete -c yes -s h -l help --description "Show help message and quit"
+complete -c yes -s V -l version --description "Show version number and quit"
