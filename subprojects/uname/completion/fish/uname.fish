c15d50be49653f6cf3cb69db1986dfab3aa41240
acly7946 <acly.work@tutanota.com>
2022-06-05 13:19:39 +0000

    Implement fish completions

--- subprojects/uname/completion/fish/uname.fish
+++ subprojects/uname/completion/fish/uname.fish
@@ -1,5 +1,11 @@
 # uname(1) completion
 
 complete -f -c uname
+complete -c uname -s a -l all --description "Print all information"
+complete -c uname -s m -l machine --description "Print machine name"
+complete -c uname -s n -l nodename --description "Print node name"
+complete -c uname -s r -l kernel-release --description "Print kernel release"
+complete -c uname -s s -l kernel-name --description "Print kernel name"
+complete -c uname -s v -l kernel-version --description "Print kernel version"
 complete -c uname -s h -l help --description "Show help message and quit"
-complete -c uname -s V -l version --description "Show the version number and quit"
+complete -c uname -s V -l version --description "Show version number and quit"
