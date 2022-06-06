6298d9c6a7f4e207115107046816e79d10ed6ee3
acly7946 <66972514+acly7946@users.noreply.github.com>
2022-06-06 10:47:34 +0000

    Add uptime subproject

---
+++ subprojects/uptime/completion/fish/uptime.fish
@@ -0,0 +1,5 @@
+# uptime(1) completion
+
+complete -f -c uptime
+complete -c uptime -s h -l help --description "Show help message and quit"
+complete -c uptime -s V -l version --description "Show version number and quit"
