6752f45afd494f5d758dd4492c74312fc92922f5
Adam Colyer <adamc@aclytop2.lan>
2022-07-04 18:33:32 +0100

    Begin implementing dirname

---
+++ subprojects/dirname/completion/fish/dirname.fish
@@ -0,0 +1,5 @@
+# dirname(1) completion
+
+complete -f -c dirname
+complete -c dirname -s h -l help --description "Show help message and quit"
+complete -c dirname -s V -l version --description "Show version number and quit"
---
+++ subprojects/dirname/src/main.c
@@ -0,0 +1,61 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void usage(void) __attribute__((noreturn));
+static void version(void) __attribute__((noreturn));
+
+int main(int argc, char *argv[])
+{
+	int optc;
+	static struct option long_options[] =
+	{
+		{"help", no_argument, NULL, 'h'},
+		{"version", no_argument, NULL, 'V'},
+	};
+
+	while((optc = getopt_long(argc, argv, "hV", long_options, NULL)) != EOF)
+	{
+		switch(optc)
+		{
+			case 'V':
+				version();
+
+			default:
+				usage();
+		}
+	}
+
+	/* string is specified */
+	while(argc > 1)
+	{
+		for(int i = 1; i < argc; i++)
+		{
+			printf("%s ", argv[i]);
+		}
+		putchar('\n');
+	}
+
+	while(argc == 1)
+	{
+		puts("y");
+	}
+
+	return EXIT_SUCCESS;
+}
+
+static void usage(void)
+{
+	fprintf(stderr,
+			"Usage: dirname [STRING] [-hV]\n"
+			"\n"
+			"  -h, --help     Show help message and quit\n"
+			"  -V, --version  Show version number and quit\n");
+	exit(EXIT_FAILURE);
+}
+
+static void version(void)
+{
+	printf("dirname %s, acly7946 coreutils\n", VERSION);
+	exit(EXIT_SUCCESS);
+}
