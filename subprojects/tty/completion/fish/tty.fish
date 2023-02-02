a8c57e5d7431d27a74103a1dc66b8f431f6c61e4
acly7946 <66972514+acly7946@users.noreply.github.com>
2023-02-02 09:40:38 +0000

    Initial commit

---
+++ subprojects/tty/completion/fish/tty.fish
@@ -0,0 +1,5 @@
+# tty(1) completion
+
+complete -f -c tty
+complete -c tty -s h -l help --description "Show help message and quit"
+complete -c tty -s V -l version --description "Show version number and quit"
---
+++ subprojects/tty/src/main.c
@@ -0,0 +1,46 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void usage(void) __attribute__((noreturn));
+static void version(void) __attribute__((noreturn));
+
+int main(int argc, char *argv[])
+{
+	int optc;
+	static struct option long_options[] =
+	{
+		{"help", no_argument, NULL, 'h'},
+		{"version", no_argument, NULL, 'V'},
+	};
+
+	while((optc = getopt_long(argc, argv, "hV", long_options, NULL)) != EOF)
+	{
+		switch(optc)
+		{
+			case 'V':
+				version();
+
+			default:
+				usage();
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
+
+static void usage(void)
+{
+	fprintf(stderr,
+			"Usage: tty [-hV]\n"
+			"\n"
+			"  -h, --help     Show help message and quit\n"
+			"  -V, --version  Show version number and quit\n");
+	exit(EXIT_FAILURE);
+}
+
+static void version(void)
+{
+	printf("tty %s, acly7946 coreutils\n", VERSION);
+	exit(EXIT_SUCCESS);
+}
