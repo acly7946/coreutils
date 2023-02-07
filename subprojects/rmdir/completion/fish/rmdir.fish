b90215db7b2211d15b72bfac3def8c32a2adf6ad
acly7946 <66972514+acly7946@users.noreply.github.com>
2023-02-07 15:20:14 +0000

    Add new utility

---
+++ subprojects/rmdir/completion/fish/rmdir.fish
@@ -0,0 +1,5 @@
+# rmdir(1) completion
+
+complete -f -c rmdir
+complete -c rmdir -s h -l help --description "Show help message and quit"
+complete -c rmdir -s V -l version --description "Show version number and quit"
