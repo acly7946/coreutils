9db3b2248952488e7f4033831110ebda3b7d2fc5
acly7946 <66972514+acly7946@users.noreply.github.com>
2023-02-08 13:10:59 +0000

    Add command line options

--- subprojects/rmdir/src/main.c
+++ subprojects/rmdir/src/main.c
@@ -12,6 +12,9 @@ int main(int argc, char *argv[])
 	static struct option long_options[] =
 	{
 		{"help", no_argument, NULL, 'h'},
+		{"ignore-fail-on-non-empty", no_argument, NULL, 'i'},
+		{"parents", no_argument, NULL, 'p'},
+		{"verbose", no_argument, NULL, 'v'},
 		{"version", no_argument, NULL, 'V'},
 	};
 
@@ -19,6 +22,15 @@ int main(int argc, char *argv[])
 	{
 		switch(optc)
 		{
+			case 'i':
+				break;
+
+			case 'p':
+				break;
+
+			case 'v':
+				break;
+
 			case 'V':
 				version();
 
@@ -35,8 +47,10 @@ static void usage(void)
 	fprintf(stderr,
 			"Usage: rmdir [-hV]\n"
 			"\n"
-			"  -h, --help     Show help message and quit\n"
-			"  -V, --version  Show version number and quit\n");
+			"  -h, --help                      Show help message and quit\n"
+			"      --ignore-fail-on-non-empty  Ignore all failures due to a non-empty directory\n"
+			"  -p, --parents                   Remove parent directories\n"
+			"  -V, --version                   Show version number and quit\n");
 	exit(EXIT_FAILURE);
 }
 
